cd616059e7438a40b0cba77a4dd9bf86d0b8dc67
Ian Johnson <ian@ianjohnson.dev>
2024-07-05 00:58:25 -0400

    feat: add SDK version argument to `flatpak-env.sh`

--- flatpak-env.sh
+++ flatpak-env.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec flatpak run --filesystem=home --share=network --share=ipc --socket=fallback-x11 --socket=wayland --device=dri --socket=session-bus org.gnome.Sdk//46
+sdk_version=${1:-46}
+exec flatpak run --filesystem=home --share=network --share=ipc --socket=fallback-x11 --socket=wayland --device=dri --socket=session-bus org.gnome.Sdk//$sdk_version
