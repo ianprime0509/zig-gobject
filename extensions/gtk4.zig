571082ff495a551a9e0e2adf86cf246e49ed23e8
Ian Johnson <ian@ianjohnson.dev>
2025-03-01 19:51:10 -0500

    fix: fix `gtk.ext.impl_helpers.bindTemplateChild`
    Fixes #95
    
    Also adds tests for `bindTemplateChild` and `bindTemplateChildPrivate`. It remains to be seen how well these tests will work when run in a CI environment (see #48), but they are useful for the current approach of manually running tests.

--- extensions/gtk4.zig
+++ extensions/gtk4.zig
@@ -24,13 +24,95 @@ pub const impl_helpers = struct {
         comptime options: gtk.ext.BindTemplateChildOptions,
     ) void {
         const field = options.field orelse name;
-        ensureWidgetType(@TypeOf(class).Instance, field);
+        const Instance = compat.typeInfo(@TypeOf(class)).pointer.child.Instance;
+        ensureWidgetType(Instance, field);
         gtk.Widget.Class.bindTemplateChildFull(
             gobject.ext.as(gtk.Widget.Class, class),
             name,
             @intFromBool(options.internal),
-            @offsetOf(@TypeOf(class).Instance, field),
+            @offsetOf(Instance, field),
         );
+    }
+
+    test bindTemplateChild {
+        const MyWidget = extern struct {
+            parent_instance: Parent,
+            label: *gtk.Label,
+
+            const template =
+                \\<?xml version="1.0" encoding="UTF-8"?>
+                \\<interface>
+                \\  <template class="BindTemplateChildTest_MyWidget" parent="GtkWidget">
+                \\    <child>
+                \\      <object class="GtkLabel" id="label">
+                \\        <property name="label">Hello, world!</property>
+                \\      </object>
+                \\    </child>
+                \\  </template>
+                \\</interface>
+                \\
+            ;
+
+            pub const Parent = gtk.Widget;
+            const Self = @This();
+
+            pub const getGObjectType = gobject.ext.defineClass(Self, .{
+                .name = "BindTemplateChildTest_MyWidget",
+                .instanceInit = &init,
+                .classInit = &Class.init,
+                .parent_class = &Class.parent,
+            });
+
+            pub fn as(widget: *Self, comptime T: type) *T {
+                return gobject.ext.as(T, widget);
+            }
+
+            pub fn new() *Self {
+                return gobject.ext.newInstance(Self, .{});
+            }
+
+            pub fn unref(widget: *Self) void {
+                gobject.Object.unref(widget.as(gobject.Object));
+            }
+
+            fn init(widget: *Self, _: *Class) callconv(.C) void {
+                gtk.Widget.initTemplate(widget.as(gtk.Widget));
+                gtk.Widget.setLayoutManager(widget.as(gtk.Widget), gtk.BinLayout.new().as(gtk.LayoutManager));
+            }
+
+            fn dispose(widget: *Self) callconv(.C) void {
+                gtk.Widget.disposeTemplate(widget.as(gtk.Widget), getGObjectType());
+                gobject.Object.virtual_methods.dispose.call(Class.parent, widget.as(Parent));
+            }
+
+            pub const Class = extern struct {
+                parent_class: Parent.Class,
+
+                var parent: *Parent.Class = undefined;
+
+                pub const Instance = Self;
+
+                pub fn as(class: *Class, comptime T: type) *T {
+                    return gobject.ext.as(T, class);
+                }
+
+                fn init(class: *Class) callconv(.C) void {
+                    gobject.Object.virtual_methods.dispose.implement(class, &dispose);
+                    gtk.ext.WidgetClass.setTemplateFromSlice(class.as(gtk.Widget.Class), template);
+                    class.bindTemplateChild("label", .{});
+                }
+
+                fn bindTemplateChild(class: *Class, comptime name: [:0]const u8, comptime options: gtk.ext.BindTemplateChildOptions) void {
+                    gtk.ext.impl_helpers.bindTemplateChild(class, name, options);
+                }
+            };
+        };
+
+        gtk.init();
+        const widget = MyWidget.new();
+        _ = gobject.Object.refSink(widget.as(gobject.Object));
+        defer widget.unref();
+        try std.testing.expectEqualStrings("Hello, world!", std.mem.span(widget.label.getLabel()));
     }
 
     /// Binds a template child to a private field.
@@ -55,6 +137,101 @@ pub const impl_helpers = struct {
             @intFromBool(options.internal),
             private_offset + @offsetOf(Private, field),
         );
+    }
+
+    test bindTemplateChildPrivate {
+        const MyWidget = extern struct {
+            parent_instance: Parent,
+
+            const template =
+                \\<?xml version="1.0" encoding="UTF-8"?>
+                \\<interface>
+                \\  <template class="BindTemplateChildPrivateTest_MyWidget" parent="GtkWidget">
+                \\    <child>
+                \\      <object class="GtkLabel" id="label">
+                \\        <property name="label">Hello, world!</property>
+                \\      </object>
+                \\    </child>
+                \\  </template>
+                \\</interface>
+                \\
+            ;
+
+            pub const Parent = gtk.Widget;
+            const Self = @This();
+
+            const Private = struct {
+                label: *gtk.Label,
+
+                var offset: c_int = 0;
+            };
+
+            pub const getGObjectType = gobject.ext.defineClass(Self, .{
+                .name = "BindTemplateChildPrivateTest_MyWidget",
+                .instanceInit = &init,
+                .classInit = &Class.init,
+                .parent_class = &Class.parent,
+                .private = .{ .Type = Private, .offset = &Private.offset },
+            });
+
+            pub fn as(widget: *Self, comptime T: type) *T {
+                return gobject.ext.as(T, widget);
+            }
+
+            pub fn new() *Self {
+                return gobject.ext.newInstance(Self, .{});
+            }
+
+            pub fn unref(widget: *Self) void {
+                gobject.Object.unref(widget.as(gobject.Object));
+            }
+
+            pub fn getLabel(widget: *Self) [:0]const u8 {
+                return std.mem.span(widget.private().label.getLabel());
+            }
+
+            fn init(widget: *Self, _: *Class) callconv(.C) void {
+                gtk.Widget.initTemplate(widget.as(gtk.Widget));
+                gtk.Widget.setLayoutManager(widget.as(gtk.Widget), gtk.BinLayout.new().as(gtk.LayoutManager));
+            }
+
+            fn dispose(widget: *Self) callconv(.C) void {
+                gtk.Widget.disposeTemplate(widget.as(gtk.Widget), getGObjectType());
+                gobject.Object.virtual_methods.dispose.call(Class.parent, widget.as(Parent));
+            }
+
+            fn private(widget: *Self) *Private {
+                return gobject.ext.impl_helpers.getPrivate(widget, Private, Private.offset);
+            }
+
+            pub const Class = extern struct {
+                parent_class: Parent.Class,
+
+                var parent: *Parent.Class = undefined;
+
+                pub const Instance = Self;
+
+                pub fn as(class: *Class, comptime T: type) *T {
+                    return gobject.ext.as(T, class);
+                }
+
+                fn init(class: *Class) callconv(.C) void {
+                    gobject.Object.virtual_methods.dispose.implement(class, &dispose);
+                    gtk.ext.WidgetClass.setTemplateFromSlice(class.as(gtk.Widget.Class), template);
+                    class.bindTemplateChildPrivate("label", .{});
+                }
+
+                fn bindTemplateChildPrivate(class: *Class, comptime name: [:0]const u8, comptime options: gtk.ext.BindTemplateChildOptions) void {
+                    gtk.ext.impl_helpers.bindTemplateChildPrivate(class, name, Private, Private.offset, options);
+                }
+            };
+        };
+
+        gtk.init();
+        const widget = MyWidget.new();
+        _ = gobject.Object.refSink(widget.as(gobject.Object));
+        defer widget.unref();
+        try std.testing.expectEqualStrings("Hello, world!", widget.getLabel());
     }
 
     fn ensureWidgetType(comptime Container: type, comptime field_name: []const u8) void {
