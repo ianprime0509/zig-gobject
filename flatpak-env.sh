d27ae7e19e49f907261711c96a70b1137e549157
Ian Johnson <ian@ianjohnson.dev>
2026-04-19 22:27:24 -0400

    ci: attempt to fix CI jobs

--- flatpak-env.sh
+++ flatpak-env.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+opts=$(getopt -o '' -l host-filesystem -n "$0" -- "$@")
+[[ $? -ne 0 ]] && exit 1
+eval set -- "$opts"
+
+filesystem_option="--filesystem=home"
+while true; do
+  case "$1" in
+    --host-filesystem) filesystem_option="--filesystem=host"; shift ;;
+    --) shift; break ;;
+    *) exit 1 ;;
+  esac
+done
+
 sdk_version=${1:-49}
-exec flatpak run --filesystem=home --share=network --share=ipc --socket=fallback-x11 --socket=wayland --device=dri --socket=session-bus org.gnome.Sdk//$sdk_version
+exec flatpak run $filesystem_option --share=network --share=ipc --socket=fallback-x11 --socket=wayland --device=dri --socket=session-bus org.gnome.Sdk//$sdk_version
